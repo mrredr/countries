79f792fc28efc0a08f1d94d6d9d42a041393244c
mrredr <mrredr@gmail.com>
2022-12-11 17:26:49 +0300

    Init project with typescript, tailwind, eslint, prettier

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="react-scripts" />
+// <reference types="react-scripts" />
+
+declare module "*.svg" {
+  import { FunctionComponent, SVGProps } from "react";
+  export const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string }>;
+
+  const src: string;
+  export default src;
+}
