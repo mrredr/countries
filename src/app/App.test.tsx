5930f632dab57001c6020155a0f166b3e63a67c9
mrredr <mrredr@gmail.com>
2022-12-19 14:13:35 +0300

    fix: delete test

--- src/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { App } from "app";
-import { HashRouter } from "react-router-dom";
-
-test("renders learn react link", () => {
-  render(<App />, { wrapper: HashRouter });
-  const loginElement = screen.getByText(/Login/i);
-  expect(loginElement).toBeInTheDocument();
-});
